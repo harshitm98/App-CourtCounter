4c481439eb23c6cea6cea07bed48797a864386d6
harshitm98 <harshitmah98@gmail.com>
2017-06-11 01:06:24 +0530

    Button adds points add to tally!

--- app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.android.courtcounter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +12,30 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    int score = 0;
+
+    public void three_points(View view){
+        score+=3;
+        displayForTeamA(score);
+    }
+
+    public void two_points(View view){
+        score+=2;
+        displayForTeamA(score);
+    }
+
+    public void free_throw(View view){
+        score+=1;
+        displayForTeamA(score);
+    }
+
+    /**
+     * Displays the given score for Team A.
+     */
+    public void displayForTeamA(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_a_score);
+        scoreView.setText(String.valueOf(score));
+    }
+
 }
